fb870d381e1e73a07137afd213b374d49569a537
lienadochuko <erhisowomedanielking@gmail.com>
2023-11-16 00:26:13 +0100

    DbContext

--- Entities/Person.cs
+++ Entities/Person.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Entities
 {
@@ -7,11 +8,21 @@ namespace Entities
         /// <summary>
         /// Person domain Model class
         /// </summary>
+
+        [Key]
         public Guid PersonID { get; set; }
+
+        [StringLength(40)]
         public string? PersonName { get; set; }
+
+        [StringLength(40)]
         public string? Email { get; set; } 
         public DateTime? DOB { get; set; }
+
+        [StringLength(10)]
         public string? Gender { get; set; }
+
+        [StringLength (200)]
         public string? Address { get; set; }
         public Guid? CountryID { get; set; }
         public bool RecieveNewsLetter { get; set; }
